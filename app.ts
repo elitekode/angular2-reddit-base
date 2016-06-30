5941644947bba8f02e211842f2831a3564a94f0b
desak29 <desak29@icloud.com>
2016-06-30 16:35:55 -0400

    Hello World

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
 // your code goes here
+import { bootstrap } from "@angular/platform-browser-dynamic"; import { Component } from "@angular/core";
+@Component({
+  selector: 'hello-world',
+  template: `
+  <div>
+    Hello world
+  </div>
+`
+})
+class HelloWorld { }
+bootstrap(HelloWorld);
+//boots the application 
